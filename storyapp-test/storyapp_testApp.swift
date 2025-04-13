a6ba068badb3bc0fd345ff1440a90ae1b3659bf1
umuryavuz96 <umuryavuz96@gmail.com>
2025-04-13 14:18:56 +0200

    Initial commit

---
+++ storyapp-test/storyapp_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  storyapp_testApp.swift
+//  storyapp-test
+//
+//  Created by Umur Yavuz on 13/4/25.
+//
+
+import SwiftUI
+
+@main
+struct storyapp_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
